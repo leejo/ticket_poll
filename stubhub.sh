e5a5c94fb4f4c8f5bbc7b92a528df5f0558d0641
Lee Johnson <lee@givengain.ch>
2023-06-17 15:59:16 +0200

    append event name so can run multiple of these
    without them interfering with each other (and to aid debugging)

--- stubhub.sh
+++ stubhub.sh
@@ -2,7 +2,7 @@
 
 #
 # usage:
-# 	stubhub.sh $no_of_tickets $max_price "$stubhub_event_url"
+# 	stubhub.sh $no_of_tickets $max_price "$stubhub_event_url" $event_name
 #
 # 	$stubhub_event_url should be of the form with the trailing slash, eg:
 # 		https://www.stubhub.com/coldplay-zurich-tickets-7-1-2023/event/150371201/
@@ -14,12 +14,14 @@ date
 PERL=/Users/leejohnson/perl5/perlbrew/perls/perl-5.36.1/bin/perl
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-TICKETS=$1
+QUANTITY=$1
 MAX_PRICE=$2
-URL="$3?quantity=$TICKETS"
+URL="$3?quantity=$QUANTITY"
+EVENT=$4
+HTML_FILE=$SCRIPT_DIR/stubhub_${EVENT}.html
 
-curl $URL > $SCRIPT_DIR/stubhub.html
-for price in $($PERL $SCRIPT_DIR/stubhub.pl --html $SCRIPT_DIR/stubhub.html --tickets $TICKETS --max-price $MAX_PRICE)
+curl $URL > $HTML_FILE
+for price in $($PERL $SCRIPT_DIR/stubhub.pl --html $HTML_FILE --tickets $QUANTITY --max-price $MAX_PRICE)
 do
-	/opt/homebrew/bin/ntfy publish tickets "$TICKETS Stubhub tickets available for $price each 😀"
+	/opt/homebrew/bin/ntfy publish tickets "$QUANTITY $EVENT Stubhub tickets available for $price each 😀"
 done
